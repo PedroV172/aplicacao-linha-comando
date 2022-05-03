2ea5e58a35bca1853dfb83a4267b171181731e72
Pedro Victor <victorpedro.biia1721@gmail.com>
2022-05-02 23:24:42 -0400

    alteração primeiro commit

--- app/app.go
+++ app/app.go
@@ -8,7 +8,7 @@ import (
 	"github.com/urfave/cli"
 )
 
-//Gerar vai retornar a aplicação de linha de comando pronta para ser executada
+//Gerar vai retornar a aplicação de linha de comando pronta para ser executada...
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
